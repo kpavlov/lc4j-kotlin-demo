fc6bf119d456d44f6d067efeaf0f5f397e9c34b1
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-06-02 09:10:05 +0300

    Update async tests with parameterized inputs and null handling (#26)
    update async tests with parameterized inputs and null handling

--- src/test/kotlin/e00/AsyncKotlinTest.kt
+++ src/test/kotlin/e00/AsyncKotlinTest.kt
@@ -2,48 +2,45 @@ package e00
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.UUID
 import kotlin.time.Duration.Companion.seconds
 
 class AsyncKotlinTest {
-    suspend fun callApi(): String {
-        delay(1.seconds) // simulate delay
-        return "Hello World"
+
+    suspend fun callApi(apiResponse: String? = "Hello World"): String? {
+        delay(1.seconds) // simulate slow operation
+        return apiResponse
     }
 
     suspend fun processData(data: String): String {
-        delay(1.seconds) // simulate delay
+        delay(1.seconds) // simulate slow operation
         return data.uppercase()
     }
 
     suspend fun saveToDb(data: String): UUID {
-        delay(1.seconds) // simulate delay
+        delay(1.seconds) // simulate slow operation
         return UUID.randomUUID()
     }
 
-    @Test
-    fun `Chain of calls`() =
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "Hello, World",
+        ]
+    )
+    @NullSource
+    fun `Chain of calls with conditions`(apiResponse: String?) =
         runBlocking {
-            val apiResult = callApi()
-            val processedData = processData(apiResult)
-            val persistentId = saveToDb(processedData)
-
-            println("✅ Result: $persistentId")
-        }
-
-    @Test
-    fun `Chain of calls with conditions`() =
-        runBlocking {
-            val persistentId: UUID?
-            val apiResult = callApi()
-            if (apiResult.contains("Bye")) {
+            val apiResult = callApi(apiResponse)
+            if (apiResult != null) {
                 val processedData = processData(apiResult)
-                persistentId = saveToDb(processedData)
+                val persistentId = saveToDb(processedData)
+                println("✅ Result: $persistentId")
             } else {
-                persistentId = null
+                println("✅ Result is null")
             }
-
-            println("✅ Result: $persistentId")
         }
 }
